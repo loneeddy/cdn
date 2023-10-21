3ea4b924d228faf455e884c78ba2a197e21601bf
loneeddy <79140384+loneeddy@users.noreply.github.com>
2023-10-21 11:44:07 +0630

    Update partfab.js

--- partfab.js
+++ partfab.js
@@ -31,20 +31,20 @@ if (!fabActive) {
 }
 $("#fab-active").html(`<div class="fab-container">
       <div class="fab">
-        <img src="https://semicon.github.io/img/logosm.png" style="width: 60px;">
+        <img src="https://loneeddy.github.io/imglink/loneeddyLogo.png" style="width: 60px;">
       </div>
   
       <ul class="fab-options">
         <li>
           <span class="fab-label">ติดตามเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://web.facebook.com/construct2n3"><i class="fa-brands fa-facebook fs-1 text-primary"></i></i></a>
+            <a target="_blank" href="https://www.facebook.com/Ksm.Mobiles"><i class="fa-brands fa-facebook fs-1 text-primary"></i></i></a>
           </div>
         </li>
         <li>
           <span class="fab-label">ติดต่อเรา</span>
           <div class="fab-icon-holder">
-            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.wichian@gmail.com&tf=cm"><i class="fas fa-envelope fs-2 text-danger"></i></a>
+            <a target="_blank" href="https://mail.google.com/mail/u/0/?fs=1&to=ph.loneeddy@gmail.com&tf=cm"><i class="fas fa-envelope fs-2 text-danger"></i></a>
           </div>
         </li>
         <li style="margin-bottom: 60px;">
